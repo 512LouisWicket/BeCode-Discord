2859d9866f38c2a1f27608b185129a19494e2061
512LouisWicket <louis.wicket@gmail.com>
2020-02-24 10:36:32 +0100

    Create first version of the command

---
+++ src/commands/anecdote.js
@@ -0,0 +1,25 @@
+"use strict";
+
+const anecdotes = new Map(require("../../data/anecdotes.json"));
+
+const rand = max => (Math.random() * max) | 0;
+
+const cmd = {
+    name: "anecdote",
+    alias: ["tips", "tip", "anecdotes"],
+    desc: "Donne une anecdote sur un langage de programmation !",
+    action({ msg, args } = {}) {
+        if (args.length) {
+            const language = anecdotes.get(args[0]);
+            if (language === void null) {
+                msg.channel.send(`Arf, aucune anecdote n’a été écrite pour le langage ${args[0]} \u{1F615}\n`);
+            } else {
+                msg.channel.send(language[rand(language.length)]);
+            }
+        } else {
+            msg.channel.send("Veuillez indiquer un langage de programmation !");
+        }
+    }
+};
+
+module.exports = cmd;
