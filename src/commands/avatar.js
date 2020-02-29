8bc9b24d1266c1b075abdbf0b94692788d11dd8e
Vincent-120 <v1996.vincent@gmail.com>
2020-02-29 11:05:17 +0100

    add author

--- src/commands/avatar.js
+++ src/commands/avatar.js
@@ -5,13 +5,11 @@ const cmd = {
     alias: ["pic", "debug"],
     desc: "Commande de test",
     action({ msg, args } = {}) {
-        console.log("action!")
-        console.log(msg.channel.guild.members)
-        
-        // console.log(msg.author)
         msg.channel.send("pseudo selectionner: "+args);
         msg.channel.send(msg.author.avatarURL);
     }
 };
 
 module.exports = cmd;
+
+//? By Vincent Vissers & Louis Wicket
